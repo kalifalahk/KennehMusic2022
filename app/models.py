01eb22fe21747115a8846b364a762f7ce1b5fcba
Kalifalah Kenneh <kkenneh@ithaca.edu>
2022-12-20 17:27:59 -0500

    Lab 6 Login

--- app/models.py
+++ app/models.py
@@ -1,6 +1,28 @@
 from sqlalchemy import DateTime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import db, login
 
-from app import db
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 
 class Artist(db.Model):
@@ -19,8 +41,6 @@ class Venue(db.Model):
     name = db.Column(db.String(120), index=True)
     location = db.Column(db.String(120), index=True)
     events = db.relationship('Event', backref='venue', lazy='dynamic')
-
-
 
     def __repr__(self):
         return '<Venue {}>'.format(self.name)
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,11 @@
 import datetime
 
 from flask import render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_user, logout_user, login_required
 from app import app, db
-from app.forms import LoginForm
-from app.models import Artist, Event, Venue, ArtistToEvent
+from app.forms import LoginForm, ArtistForm, RegistrationForm
+from app.models import Artist, Event, Venue, ArtistToEvent, User
+from werkzeug.urls import url_parse
 
 
 @app.route('/')
@@ -26,7 +28,48 @@ def artist(name):
     return render_template('artist.html', title=art.name, artist=art)
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+
 @app.route('/new_artist', methods=['GET', 'POST'])
+@login_required
 def new_artist():
     if request.method == 'POST':
         artist_name = request.form["name"]
@@ -40,7 +83,7 @@ def new_artist():
         db.session.commit()
         return render_template('artists.html', title='Artists', artists=Artist.query.all())
     else:
-        form = LoginForm()
+        form = ArtistForm()
         return render_template('newartist.html', form=form)
 
 
