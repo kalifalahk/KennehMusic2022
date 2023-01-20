4acd5f894034dcd3212527fafba3be8c28bef886
Kalifalah Kenneh <kkenneh@ithaca.edu>
2023-01-20 02:33:24 -0500

    Lab 7 complete

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 import datetime
 
-from flask import render_template, flash, redirect, url_for, request
+from flask import render_template, flash, redirect, url_for, request,jsonify
 from flask_login import current_user, login_user, logout_user, login_required
 from app import app, db
 from app.forms import LoginForm, ArtistForm, RegistrationForm, VenueForm, EventForm
