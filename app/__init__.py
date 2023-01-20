8910254361b98d9905419118c4fe213563f2bcf8
Kalifalah Kenneh <kkenneh@ithaca.edu>
2023-01-19 21:52:18 -0500

    Lab 7 complete

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_bootstrap import Bootstrap
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -10,5 +12,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+bootstrap = Bootstrap(app)
 
-from app import routes, models
+
+from app import routes, models, errors
